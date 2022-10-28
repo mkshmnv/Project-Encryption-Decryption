d844cf44bc2c952cc9c75155cc8c88bd98ae1a4a
MyIDDQD <nickkosh@gmail.com>
2022-10-28 17:32:24 +0200

    Done - Initializing an array

--- Topics/Arrays/Initializing an array/src/Main.kt
+++ Topics/Arrays/Initializing an array/src/Main.kt
@@ -1,5 +1,8 @@
 fun main() {
-    val numbers = // put your code here
+    val numbers = IntArray(100) { 0 }
+    numbers[0] = 1
+    numbers[9] = 10
+    numbers[99] = 100
 
     // do not touch the lines below
     println(numbers.joinToString())
