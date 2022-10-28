8722505b7c1841fa2ee67ab23c03b63640d64f44
MyIDDQD <nickkosh@gmail.com>
2022-10-28 13:58:13 +0200

    Done project. Stage 3/6: Decrypted

--- Encryption-Decryption/task/src/Main.kt
+++ Encryption-Decryption/task/src/Main.kt
@@ -1,22 +1,67 @@
-fun main() {
-    val message = readln().lowercase()
-    val decryptKey = readln().toInt()
-    val alphabets = "abcdefghijklmnopqrstuvwxyz"
+const val ENCRYPTION = "enc"
+const val DECRYPTION = "dec"
+fun encryption(message: String, key: Int): String {
     var encryptMessage = ""
-
     for (letter in message) {
-        encryptMessage += if (alphabets.contains(letter)) {
-            val tempIndex = alphabets.indexOf(letter) + decryptKey
-            if (tempIndex > alphabets.length - 1) {
-                alphabets[tempIndex - alphabets.length]
-            } else {
-                alphabets[tempIndex]
-            }
-        } else {
-            letter
-        }
+        encryptMessage += (letter.code + key).toChar()
     }
-    println(encryptMessage)
+    return encryptMessage
+}
+
+fun decryption(message: String, key: Int): String {
+    var encryptMessage = ""
+    for (letter in message) {
+        encryptMessage += (letter.code - key).toChar()
+    }
+    return encryptMessage
+}
+
+fun main() {
+    val keyCryption = readln().lowercase()
+    val inputMessage = readln()
+    val inputKey = readln().toInt()
+    encryption(inputMessage, inputKey)
+
+    println(
+        when (keyCryption) {
+            ENCRYPTION -> {
+                encryption(inputMessage, inputKey)
+            }
+
+            DECRYPTION -> {
+                decryption(inputMessage, inputKey)
+            }
+
+            else -> {
+                println("Error!")
+            }
+
+        }
+    )
+}
+
+
+
+
+// Stage 2/6
+//    val message = readln().lowercase()
+//    val decryptKey = readln().toInt()
+//    val alphabets = "abcdefghijklmnopqrstuvwxyz"
+//    var encryptMessage = ""
+//
+//    for (letter in message) {
+//        encryptMessage += if (alphabets.contains(letter)) {
+//            val tempIndex = alphabets.indexOf(letter) + decryptKey
+//            if (tempIndex > alphabets.length - 1) {
+//                alphabets[tempIndex - alphabets.length]
+//            } else {
+//                alphabets[tempIndex]
+//            }
+//        } else {
+//            letter
+//        }
+//    }
+//    println(encryptMessage)
 
 // Stage 1/6
 //    val message = "we found a treasure!"
@@ -31,4 +76,3 @@ fun main() {
 //        }
 //    }
 //    println(encryptMessage)
-}
