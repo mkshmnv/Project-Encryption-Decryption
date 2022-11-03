ceb6af69355fd793c8de35fdb48770e5f84e5ae0
MyIDDQD <nickkosh@gmail.com>
2022-11-03 21:23:27 +0100

    Interim Stage 5/6: X-files

--- Encryption-Decryption/task/src/Main.kt
+++ Encryption-Decryption/task/src/Main.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 // Make global constant
 const val ENCRYPTION = "enc"
 const val DECRYPTION = "dec"
@@ -5,99 +7,53 @@ var MODE = DECRYPTION
 var KEY = 0
 var DATA = ""
 
-// Perform encryption and decryption
-fun encryption(message: String, key: Int): String {
+fun main(o: Array<String>) {
+
+    val one = arrayOf("-mode", "enc", "-key", "5", "-data", "Welcome to hyperskill!", "-out", "output.txt")
+    val args = arrayOf("-mode", "enc", "-key", "5", "-data", "Welcome to hyperskill!")
+    val third = arrayOf("-key", "5", "-data", "\\jqhtrj%yt%m~ujwxpnqq&", "-mode", "dec")
+
+    values(args)
+
+    if ("-in" in args && "-out" in args) {
+        val myFile = File(args[args.indexOf("-out") + 1])
+        myFile.writeText(crypt(DATA, KEY))
+    } else if ("-data" in args || "-in" in args) {
+        println(crypt(DATA, KEY))
+    }
+}
+
+//    Parsing input information
+fun values (inputArray: Array<String>) {
+    when {
+        ("-mode" in inputArray) -> MODE = DECRYPTION
+        ("-key" in inputArray) -> KEY = inputArray[inputArray.indexOf("-key") + 1].toInt()
+        ("-data" in inputArray) -> DATA = inputArray[inputArray.indexOf("-data") + 1]
+//        ("-in" in inputArray && "-data" !in inputArray) -> {
+//            val fileName = inputArray[inputArray.indexOf("-in") + 1]
+//            val file = File(fileName)
+//            DATA = file.readText()
+//        }
+    }
+}
+
+// Encryption and decryption
+fun crypt(message: String, key: Int): String {
     var encryptMessage = ""
-    for (letter in message) {
-        encryptMessage += (letter.code + key).toChar()
+    when (MODE) {
+        ENCRYPTION -> {
+            for (letter in message) {
+                encryptMessage += (letter.code + key).toChar()
+            }
+        }
+        DECRYPTION -> {
+            for (letter in message) {
+                encryptMessage += (letter.code - key).toChar()
+            }
+        }
+        else -> {
+            throw Exception("Error")
+        }
     }
     return encryptMessage
 }
-
-fun decryption(message: String, key: Int): String {
-    var encryptMessage = ""
-    for (letter in message) {
-        encryptMessage += (letter.code - key).toChar()
-    }
-    return encryptMessage
-}
-
-// Verify input information
-fun checkMode(mode: String) {
-    MODE = (if (mode == DECRYPTION) DECRYPTION else ENCRYPTION).toString()
-}
-
-fun checkKey(key: Int?) {
-    KEY = key ?: 0
-}
-
-fun checkData(data: String?) {
-    DATA = data ?: ""
-}
-
-fun clarification(inputArray: Array<String>) {
-    for ((index, value) in inputArray.withIndex()) {
-        when {
-            value == "-mode" -> checkMode(inputArray[index + 1])
-            value == "-key" -> checkKey(inputArray[index + 1].toInt())
-            value == "-data" -> checkData(inputArray[index + 1])
-        }
-    }
-}
-
-fun main(args: Array<String>) {
-
-    // Clarification input information
-    clarification(args)
-
-    // Initiate encryption and decryption
-    println(
-        when (MODE) {
-            ENCRYPTION -> {
-                encryption(DATA, KEY)
-            }
-
-            DECRYPTION -> {
-                decryption(DATA, KEY)
-            }
-
-            else -> {
-                println("Error!")
-            }
-        }
-    )
-}
-
-// Stage 2/6
-//    val message = readln().lowercase()
-//    val decryptKey = readln().toInt()
-//    val alphabets = "abcdefghijklmnopqrstuvwxyz"
-//    var encryptMessage = ""
-//
-//    for (letter in message) {
-//        encryptMessage += if (alphabets.contains(letter)) {
-//            val tempIndex = alphabets.indexOf(letter) + decryptKey
-//            if (tempIndex > alphabets.length - 1) {
-//                alphabets[tempIndex - alphabets.length]
-//            } else {
-//                alphabets[tempIndex]
-//            }
-//        } else {
-//            letter
-//        }
-//    }
-//    println(encryptMessage)
-
-// Stage 1/6
-//    val message = "we found a treasure!"
-//    var encryptMessage = ""
-//    val alphabets = ('a'..'z').toList()
-//
-//    for (letterMessage in message.lowercase()) {
-//        encryptMessage += if (alphabets.contains(letterMessage)) {
-//            alphabets.reversed()[alphabets.indexOf(letterMessage)]
-//        } else {
-//            letterMessage
-//        }
-//    }
-//    println(encryptMessage)
